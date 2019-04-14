7b7f89f53ee6849550d293cfed09d2f74428918d
flipbarrett <ovd.phil@gmail.com>
2019-04-14 10:56:21 +0000

    added views directory with home.ejs in it, updated app.js with express body parser mongoose ejs and flash

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const express = require("express"),
-	  app	  = express();
+const express     = require("express"),
+	  app	      = express(),
+ 	  bodyParser  = require("body-parser"),
+	  mongoose    = require("mongoose"),
+	  flash       = require("connect-flash"); 
+
+app.use(bodyParser. urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(flash());
+	  app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The Forge is lit."); 
+});
